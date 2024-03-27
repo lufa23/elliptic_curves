d36063a2f8e702dac2e3922d46530ea143b10293
lufa23 <usuario@mind-your-business.whatever>
2024-03-27 11:38:35 +0100

    implement points addition

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use num_bigint::{BigUint};
 
-struct Point {
-    x: BigUint,
-    y: BigUint,
+#[derive(PartialEq, Clone)]
+enum Point {
+    Coordinate(BigUint, BigUint),
+    Identity,
 }
 struct EllipticCurve {
     // y² = x² + a*x +b
@@ -12,8 +13,32 @@ struct EllipticCurve {
 }
 
 impl EllipticCurve {
-    fn add(j: &Point, k: &Point) -> Point {
-        todo!()
+    pub fn add(&self, j: &Point, k: &Point) -> Point {
+        assert!(self.is_on_curve(j), "Point does not belong to curve");
+        assert!(self.is_on_curve(k), "Point does not belong to curve");
+        assert!(*j != *k, "Points are identical.");
+        match (j, k) {
+            (Point::Identity,_) => k.clone(),
+            (_, Point::Identity) => j.clone(),
+            (Point::Coordinate(x1, y1) , Point::Coordinate(x2, y2)) => {
+                // s = (y2 -y1) / (x2 -x1)
+                // x3 = s² -x1 -x2
+                // y³ = s (x1 -x3) -y1 mod p
+                let y2minusy1 = (FiniteField::subtract(y2, y1, &self.p));
+                let x2minusx1 = (FiniteField::subtract(x2, x1, &self.p));
+                let s = FiniteField::divide(&y2minusy1, &x2minusx1, &self.p);
+                let s2 = s.modpow(&BigUint::from(2u32), &self.p);
+                let s2minusx1 = FiniteField::subtract(&s2, &x1, &self.p);
+                let x3 = FiniteField::subtract(&s2minusx1, x2, &self.p);
+                let x1minusx3 = FiniteField::subtract(x1, &x3, &self.p);
+                let sx1minusx3 = FiniteField::mul(&s, &x1minusx3, &self.p);
+                let y3 = FiniteField::subtract(&sx1minusx3, &y1, &self.p);
+                Point::Coordinate(x3, y3)
+
+
+
+            }
+        }
     }
 
     fn double(j: &Point) -> Point {
@@ -25,6 +50,22 @@ impl EllipticCurve {
         //addition/doubling algorithm
         // B = g* A
         todo!()
+    }
+
+
+    fn is_on_curve(&self, j: &Point) -> bool {
+        // y² = x³ + a*x + b
+        match j {
+            Point::Coordinate(x, y ) => {
+                let y2 = y.modpow (&BigUint::from(2u32), &self.p);
+                let x3 = x.modpow(&BigUint::from(3u32), &self.p);
+                let ax = &self.a * x;
+                y2 == x3 +ax + &self.b
+            }
+
+            Point::Identity => true,
+        } 
+
     }
 
 }
@@ -54,10 +95,21 @@ impl FiniteField {
         p - j
     }
 
+
+    pub fn subtract(j: &BigUint, k: &BigUint, p: &BigUint) -> BigUint {
+        let k_inv = FiniteField::inv_addition(k, p);
+        FiniteField::add(j, &k_inv, p)
+    }
+
     pub fn inv_multiplication(j: &BigUint, p: &BigUint) -> BigUint {
         // TODO: this function is only valid for a p prime
         // c^(-1) mod p = c^(p-2) mod p 
         j.modpow(&(p - BigUint::from(2u32)), p)
+    }
+
+    pub fn divide(j: &BigUint, k: &BigUint, p: &BigUint) -> BigUint {
+        let k_inv = FiniteField::inv_multiplication(k, p);
+        FiniteField::add (j, &k_inv, p)
     }
 
 
