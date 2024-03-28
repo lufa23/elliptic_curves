d90ccbd0c71ec1106f19801fcc9d4bf10da009b5
lufa23 <usuario@mind-your-business.whatever>
2024-03-28 12:40:22 +0100

    added secp256k1 example

--- src/lib.rs
+++ src/lib.rs
@@ -89,7 +89,7 @@ impl EllipticCurve {
         // if bit i of d == 1
         // T = T + A
         let mut t = j.clone();
-        for i in 0..(k.bits() - 1) {
+        for i in (0..(k.bits() - 1)).rev() {
             t = self.double(&t);
             if k.bit(i) {
                 t = self.add(&t, j);
@@ -372,5 +372,51 @@ mod tests {
         let pr = Point::Coordinate(BigUint::from(7u32), BigUint::from(11u32));
         let res: Point = ec.scalar_mul(&j, &BigUint::from(10u32));
         assert_eq!(res, pr);
+    }
+
+
+    #[test]
+    fn test_ec_secp256k1() {
+        // p = FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE FFFFFC2F
+        // a = 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+        // b = 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000007
+        // G = 04 79BE667E F9DCBBAC 55A06295 CE870B07 029BFCDB 2DCE28D9 59F2815B 16F81798 483ADA77 26A3C465 5DA4FBFC 0E1108A8 FD17B448 A6855419 9C47D08F FB10D4B8
+        // n = FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE BAAEDCE6 AF48A03B BFD25E8C D0364141
+        //
+
+        let p = BigUint::parse_bytes(
+            b"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F",
+            16
+        ).expect("could not convert p");
+
+        let n = BigUint::parse_bytes(
+            b"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
+            16
+        ).expect("could not convert n");
+
+        let gx = BigUint::parse_bytes(
+            b"79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 
+            16
+        ).expect("could not convert gx");
+
+        let gy = BigUint::parse_bytes(
+            b"483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 
+            16
+        ).expect("could not convert gy");
+
+
+        let ec = EllipticCurve {
+            a: BigUint::from(0u32),
+            b: BigUint::from(7u32),
+            p,
+
+        };
+
+        let g = Point::Coordinate((gx), (gy));
+        let res = ec.scalar_mul(&g, &n,); // n * G
+
+        assert_eq!(res, Point::Identity);
+        
+
     }
 }
